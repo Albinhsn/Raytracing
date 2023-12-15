850aab4a6fe46c389212bacb09300b410641b12f
albinhsn <albin.henningsson1@gmail.com>
2023-12-15 21:01:22 +0100

    chapter 12

--- camera.h
+++ camera.h
@@ -19,6 +19,11 @@ class camera {
         int samples_per_pixel = 10;
         int max_depth = 10;
 
+        double vfov = 90;
+        point3 lookfrom = point3(0, 0, -1);
+        point3 lookat = point3(0, 0, 0);
+        vec3 vup = vec3(0, 1, 0);
+
         void render(const hittable &world) {
             initialize();
 
@@ -27,8 +32,10 @@ class camera {
 
             for (int j = 0; j < image_height; j++) {
                 std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
+
                 for (int i = 0; i < image_width; i++) {
                     color pixel_color(0, 0, 0);
+
                     for (int sample = 0; sample < samples_per_pixel; ++sample) {
                         ray r = get_ray(i, j);
                         pixel_color += ray_color(r, max_depth, world);
@@ -46,6 +53,7 @@ class camera {
         point3 pixel00_loc;
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
+        vec3 u, v, w;
 
         ray get_ray(int i, int j) const {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
@@ -67,20 +75,26 @@ class camera {
             image_height = static_cast<int>(image_width / aspect_ratio);
             image_height = (image_height < 1) ? 1 : image_height;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
             // Determine viewport dimension
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto focal_length = (lookfrom - lookat).length();
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta / 2);
+            auto viewport_height = 2 * h * focal_length;
             auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
+            vec3 viewport_u = viewport_width * u;
+            vec3 viewport_v = viewport_height * -v;
 
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
-            auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+            auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 

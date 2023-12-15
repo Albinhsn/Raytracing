c388d75d5f1b9e42100876a0dcd87f35b2096752
albinhsn <albin.henningsson1@gmail.com>
2023-12-15 10:43:25 +0100

    Chapter 8 Anti aliasing

--- color.h
+++ color.h
@@ -1,16 +1,24 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 #include <iostream>
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel_color) {
-  out << static_cast<int>(255.999 * pixel_color.x()) << " "
-      << static_cast<int>(255.999 * pixel_color.y()) << " "
-      << static_cast<int>(255.999 * pixel_color.z()) << "\n";
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+    auto scale = 1.0 / samples_per_pixel;
+
+    auto r = pixel_color.x() * scale;
+    auto g = pixel_color.y() * scale;
+    auto b = pixel_color.z() * scale;
+
+    static const interval intensity(0.000, 0.999);
+
+    out << static_cast<int>(256 * intensity.clamp(r)) << " " << static_cast<int>(256 * intensity.clamp(g)) << " "
+        << static_cast<int>(256 * intensity.clamp(b)) << "\n";
 }
 
 #endif
--- rtweekend.h
+++ rtweekend.h
@@ -1,25 +1,24 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
-#include <cmath>
-#include <limits>
-#include <memory>
 #include "ray.h"
 #include "vec3.h"
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
 
-
-using std::shared_ptr;
 using std::make_shared;
+using std::shared_ptr;
 using std::sqrt;
-
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
+inline double degrees_to_radians(double degrees) { return degrees * pi / 100.0; }
 
-inline  double degrees_to_radians(double degrees){
-  return degrees * pi / 100.0;
-}
+inline double random_double() { return rand() / (RAND_MAX + 1.0); }
 
+inline double random_double(double min, double max) { return min + (max - min) * random_double(); }
 
 #endif
